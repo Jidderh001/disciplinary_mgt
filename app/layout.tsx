f8f29343ee2595a9896d912513e386d770856edf
Jidderh001 <hauwadanwanka@gmail.com>
2025-07-14 13:25:01 +0100

    Resolved layout.tsx merge conflict

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,10 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-<<<<<<< HEAD
+  
   title: 'Disciplinary System',
   description: 'created for managing student disciplinary actions',
   
-=======
-  title: "Disciplinary System Dashboard", // Change this line
-  description: "Created with v0",
-    generator: 'v0.dev'
->>>>>>> 5e84ce4bbdf3fe1e97b7bb09286fff8e6e567135
 }
 
 export default function RootLayout({
